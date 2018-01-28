b913673cefc3a0b544ad9ef0af969ef23a5a5bf8
Khue Quang Nguyen <khuenq.devmail@gmail.com>
2018-01-28 09:04:14 +0200

    Add verification controller

---
+++ src/AppBundle/Controller/VerificationController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class VerificationController extends Controller
+{
+
+    public function showImageAction()
+    {
+
+    }
+
+    public function markAsCorrectAction()
+    {
+
+    }
+
+    public function markAsIncorrectAction()
+    {
+
+    }
+
+    public function ignoreAction()
+    {
+
+    }
+
+}
