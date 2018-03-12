2d0588290a0dae51638c07291b951f74f60caede
Khue Quang Nguyen <khue.nguyen@optima.fi>
2018-03-12 16:55:13 +0200

    Update google map markers

--- web/assets/js/googlemap.js
+++ web/assets/js/googlemap.js
@@ -1,27 +1,30 @@
 var map;
+var markers = [];
+var lastInfoWindow;
 
+/**
+ * Initialize map data
+ */
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: 65.397, lng: -20.644},
+        zoom: 6
     });
 
-    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-
-    google.maps.event.addListener(map, 'bounds_changed', function() {
-        var bounds = map.getBounds();
-        var ne = bounds.getNorthEast();
-        var sw = bounds.getSouthWest();
-        var nw = new google.maps.LatLng(ne.lat(), sw.lng());
-        var se = new google.maps.LatLng(sw.lat(), ne.lng());
-
-        retrieveImages(nw.lat(), sw.lat(), nw.lng(), ne.lng());
+    google.maps.event.addListener(map, 'tilesloaded', function () {
+        setMarkers(map);
     });
-    //console.log(bounds);
 }
 
+/**
+ * Retrieve images from API
+ *
+ * @param minLat
+ * @param maxLat
+ * @param minLng
+ * @param maxLng
+ */
 function retrieveImages(minLat, maxLat, minLng, maxLng) {
-    //console.log(minLat, maxLat, minLng, maxLng);
     jQuery.ajax({
         url: jQuery("#list-api-url").val(),
         dataType: 'json',
@@ -31,8 +34,68 @@ function retrieveImages(minLat, maxLat, minLng, maxLng) {
             min_lng: minLng,
             max_lng: maxLng
         },
-        done: function(data) {
-            console.log(data);
+        success: function (data) {
+            clearOldMarkers();
+            addMarkersFromData(data);
         }
     });
+}
+
+function clearOldMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+
+    markers = [];
+}
+
+function addMarkersFromData(data) {
+    for (var i = 0; i < data.length; i++) {
+        addMarker({
+            lat: data[i].latitude,
+            lng: data[i].longitude
+        }, data[i]);
+    }
+}
+
+function addMarker(position, image) {
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: image.address
+    });
+
+    var imageContent = '<div class="content">' +
+        '<img src="' + image.src + '" alt="' + image.alt + '" width="100px"/>' +
+        '</div>';
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: imageContent
+    });
+
+    marker.addListener('click', function() {
+        if(lastInfoWindow !== undefined) {
+            lastInfoWindow.close();
+        }
+
+        infoWindow.open(map, marker);
+        lastInfoWindow = infoWindow;
+    });
+
+    markers.push(marker);
+}
+
+/**
+ * Set map markers
+ *
+ * @param map
+ */
+function setMarkers(map) {
+    var bounds = map.getBounds();
+    var ne = bounds.getNorthEast();
+    var sw = bounds.getSouthWest();
+    var nw = new google.maps.LatLng(ne.lat(), sw.lng());
+    // var se = new google.maps.LatLng(sw.lat(), ne.lng());
+
+    retrieveImages(sw.lat(), nw.lat(), nw.lng(), ne.lng());
 }
