c37d8dc6381dd06b05d2f2d683a780eab1e6b82e
Khue Quang Nguyen <khue.nguyen@optima.fi>
2018-04-08 12:00:55 +0300

    RC-1: Update modal windows

--- web/assets/js/googlemap.js
+++ web/assets/js/googlemap.js
@@ -78,7 +78,6 @@ function addMarkers(data, map) {
             // optional
             obj.name = 'marker_' + j;
             obj.thumburl = data[j].thumbnail;
-            //obj.photourl = data[j].photourl;
             obj.color = "yellow";
 
             clusteringObj.addObject(obj); // adds object to the array markersData of clusteringObj
@@ -114,10 +113,38 @@ function buildImageContent(data) {
 
     for (var i = 0; i < data.length; i++) {
         var image = data[i];
+        var editRoute = Routing.generate('gallery_edit', {'id': image.id}, false);
+        var deleteRoute = Routing.generate('gallery_delete', {'id': image.id}, false);
 
-        itemHtml += '<a class="carousel-item" href="#image' + i + '!">' +
-            '<img src="' + getImcImage(image) + '" alt="' + image.alt + '" class="map-image" data-id="' + image.id + '"/>' +
-            '</a>';
+        itemHtml += '<div class="col s4">' +
+            '<div class="card hoverable sticky-action large">' +
+            '<div class="card-image waves-effect waves-block waves-light">' +
+            '<img src="' + getImcImage(image) + '" alt="' + image.alt + '" class="gallery-image activator" />\n' +
+            '<span class="card-title activator">' + (image.address ? image.address : '') + '</span>' +
+            '<a class="btn-floating halfway-fab waves-effect waves-light light-blue btn-edit" href="' + editRoute + '">' +
+            '<i class="material-icons">edit</i>' +
+            '</a>' +
+            '<a class="btn-floating halfway-fab waves-effect waves-light red btn-delete" href="' + deleteRoute + '">' +
+            '<i class="material-icons">delete</i>' +
+            '</a>' +
+            '</div>' +
+            '<div class="card-content">' +
+            '<p><b>' + image.alt + '</b><i class="card-title activator material-icons right">more_vert</i></p>' +
+            '<p class="truncate">' + image.description + '</p>' +
+            '</div>' +
+            '<div class="card-reveal">' +
+            '<span class="row card-title activator">Image details<i class="material-icons right">close</i></span>' +
+            '<div class="row"><div class="col s6"><b>Description</b></div><div class="col s6">' + image.description + '</div></div>' +
+            '<div class="row"><div class="col s6"><b>Type</b></div><div class="col s6">' + image.type + '</div></div>' +
+            '<div class="row"><div class="col s6"><b>Size</b></div><div class="col s6">' + image.width + 'x' + image.height + ' px</div></div>' +
+            '<div class="row"><div class="col s6"><b>Address</b></div><div class="col s6">' + (image.address ? image.address : 'N/A') + '</div></div>' +
+            '<div class="row"><div class="col s6"><b>Latitude</b></div><div class="col s6">' + (image.latitude ? parseFloat(image.latitude).toFixed(6) : 'N/A') + '</div></div>' +
+            '<div class="row"><div class="col s6"><b>Longitude</b></div><div class="col s6">' + (image.latitude ? parseFloat(image.longitude).toFixed(6) : 'N/A') + '</div></div>' +
+            '<div class="row"><div class="col s6"><b>URL</b></div><div class="col s6">' + image.src + '</div></div>' +
+            '<div class="row"><div class="col s6"><b>Author</b></div><div class="col s6">' + (image.image_metadata.author ? image.image_metadata.author : 'N/A') + '</div></div>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
     }
 
     return itemHtml;
@@ -138,20 +165,8 @@ function openImageWindow(position) {
             if (data.length > 0) {
                 var imageContent = buildImageContent(data);
 
-                $('#image-container').html('<div class="carousel">'
-                    + imageContent
-                    + '</div>'
-                );
-
+                $('#image-container').html(imageContent);
                 $('#modal').modal("open");
-
-                $('.carousel').carousel({
-                    onCycleTo: function (el, dragged) {
-                        getImageInfoHtml(el.find('img').data('id'));
-                    }
-                });
-
-                getImageInfoHtml(data[0].id);
             }
         }
     });
