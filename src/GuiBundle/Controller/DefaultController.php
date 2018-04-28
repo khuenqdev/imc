0613f6d6f6f53583e0bf0ae2a7f26ee51b78f7e5
Khue Quang Nguyen <khue.nguyen@optima.fi>
2018-04-28 12:47:37 +0300

    Fix cache permissions

--- src/GuiBundle/Controller/DefaultController.php
+++ src/GuiBundle/Controller/DefaultController.php
@@ -25,6 +25,7 @@ class DefaultController extends Controller
      * General Statistics
      *
      * @return Response
+     * @throws \Doctrine\ORM\ORMException
      */
     public function statsAction()
     {
@@ -43,6 +44,7 @@ class DefaultController extends Controller
      * Address Statistics
      *
      * @return Response
+     * @throws \Doctrine\ORM\ORMException
      */
     public function addressStatsAction()
     {
@@ -56,6 +58,7 @@ class DefaultController extends Controller
      * Geoparsing Statistics
      *
      * @return Response
+     * @throws \Doctrine\ORM\ORMException
      */
     public function geoparsingStatsAction()
     {
@@ -71,6 +74,7 @@ class DefaultController extends Controller
     public function testAction()
     {
         $image = $this->getManager()->getRepository(Image::class)->find(3);
+
         return $this->render(
             '@Gui/gui/test.html.twig',
             [
@@ -89,15 +93,63 @@ class DefaultController extends Controller
         $kernel = $this->get('kernel');
         $application = new \Symfony\Bundle\FrameworkBundle\Console\Application($kernel);
         $application->setAutoExit(false);
-        $options = array('command' => 'cache:clear',"--env" => 'prod', '--no-warmup' => true);
 
         try {
+            $options = array('command' => 'cache:clear', "--env" => 'prod', '--no-warmup' => true);
             $application->run(new \Symfony\Component\Console\Input\ArrayInput($options));
+            $options = array('command' => 'cache:clear', "--env" => 'prod', '--no-warmup' => true);
+            $application->run(new \Symfony\Component\Console\Input\ArrayInput($options));
+
+            // Set cache permission
+            $this->chmodRecursive($this->container->get('kernel')->getRootDir() . '/cache');
         } catch (\Exception $e) {
             return new Response($e->getMessage());
         }
 
         return new Response('Cache cleared!');
+    }
+
+    /**
+     * Recursively set permission
+     *
+     * @param $dir
+     */
+    private function chmodRecursive($dir)
+    {
+        $dp = opendir($dir);
+
+        while ($file = readdir($dp)) {
+            if (($file == ".") || ($file == "..")) continue;
+
+            $path = $dir . "/" . $file;
+            $isDir = is_dir($path);
+
+            $this->setPerms($path, $isDir);
+            if ($isDir) $this->chmodRecursive($path);
+        }
+
+        closedir($dp);
+    }
+
+    /**
+     * Set permissions for directory or file
+     *
+     * @param $file
+     * @param $isDir
+     */
+    private function setPerms($file, $isDir)
+    {
+        $perm = substr(sprintf("%o", fileperms($file)), -4);
+        $dirPermissions = "0750";
+        $filePermissions = "0644";
+
+        if ($isDir && $perm != $dirPermissions) {
+            chmod($file, octdec($dirPermissions));
+        } else if (!$isDir && $perm != $filePermissions) {
+            chmod($file, octdec($filePermissions));
+        }
+
+        flush();
     }
 
     /**
